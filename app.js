c7857362e2332c16fc575ecda5c9f2e40e709bfe
mohinimishra <asmyselfmohini@gmail.com>
2020-12-05 15:11:17 +0530

    added vue mini project , install vuetify

--- app.js
+++ app.js
@@ -1,111 +1,124 @@
-new Vue({
-    el : "#root",
-    el : "#game",
-    data : {
-        name : "",
-        link : "https://mohinimishra.github.io/",
-        value : 23,
-        x : 0,
-        y:0,
-        age : 20,
-        a : 0,
-        b : 0,
-        avilable : false,
-        toggle : false,
-        array : ['Joe','Jack','Joo'],
-        obj : {name : 'Joe', age : 24},
-        arrObj :[{name : 'Joe', age : 24},{name : 'Jack', age : 23},{name : 'Joo', age : 19}],
-        health : 100,
-        ended : false
-    },
-    methods : {
-        greet : function(time){
-            return `Hii ${this.name} Good ${time}`
-        },
+// new Vue({
+//     el: "#root",
+//     el: "#game",
+//     data: {
+//         name: "",
+//         link: "https://mohinimishra.github.io/",
+//         value: 23,
+//         x: 0,
+//         y: 0,
+//         age: 20,
+//         a: 0,
+//         b: 0,
+//         avilable: false,
+//         toggle: false,
+//         array: ['Joe', 'Jack', 'Joo'],
+//         obj: { name: 'Joe', age: 24 },
+//         arrObj: [{ name: 'Joe', age: 24 }, { name: 'Jack', age: 23 }, { name: 'Joo', age: 19 }],
+//         health: 100,
+//         ended: false
+//     },
+//     methods: {
+//         greet: function (time) {
+//             return `Hii ${this.name} Good ${time}`
+//         },
 
-        add : function(inc){
-            return this.value += inc
-        }, 
-        substract : function(dec){
-            return this.value -= dec
-        },
-        updateXY : function(event){
-            this.x =event.offsetX;
-            this.y =  event.offsetY;
-        },
-        logName : function(){
-            alert('you eneterd your name')
-        },
-        logAge : function(){
-            alert('you eneterd your Age')
-        },
-        // addToA : function(){
-        //     console.log('Add to A')
-        //     return this.age + this.a
-        // },
-        // addToB : function(){
-        //     console.log('Add to B')
-        //     return this.age + this.b
-        // }
-        punch : function(){
-            this.health -= 10
-            if(this.health<=0){
-                this.ended = true
-            }
-        },
-        restart : function(){
-            this.health = 100
-            this.ended = false
-        }
-    },
-    computed: {
-        addToA : function(){
-            console.log('Add to A')
-            return this.age + this.a
-        },
-        addToB : function(){
-            console.log('Add to B')
-            return this.age + this.b
-        },
-        compClass : function(){
-            return {
-                avilable : this.avilable,
-                toggle : this.toggle
-            }
-        }
-    }
-});
+//         add: function (inc) {
+//             return this.value += inc
+//         },
+//         substract: function (dec) {
+//             return this.value -= dec
+//         },
+//         updateXY: function (event) {
+//             this.x = event.offsetX;
+//             this.y = event.offsetY;
+//         },
+//         logName: function () {
+//             alert('you eneterd your name')
+//         },
+//         logAge: function () {
+//             alert('you eneterd your Age')
+//         },
+//         // addToA : function(){
+//         //     console.log('Add to A')
+//         //     return this.age + this.a
+//         // },
+//         // addToB : function(){
+//         //     console.log('Add to B')
+//         //     return this.age + this.b
+//         // }
+//         punch: function () {
+//             this.health -= 10
+//             if (this.health <= 0) {
+//                 this.ended = true
+//             }
+//         },
+//         restart: function () {
+//             this.health = 100
+//             this.ended = false
+//         }
+//     },
+//     computed: {
+//         addToA: function () {
+//             console.log('Add to A')
+//             return this.age + this.a
+//         },
+//         addToB: function () {
+//             console.log('Add to B')
+//             return this.age + this.b
+//         },
+//         compClass: function () {
+//             return {
+//                 avilable: this.avilable,
+//                 toggle: this.toggle
+//             }
+//         }
+//     }
+// });
 
-// Component
-Vue.component('greeting',{
-    template : '<p>Hello I am {{name}} <button v-on:click="changeName">Click ME</button></p> ',
-    data : function(){
-        return{
-            name : 'MM'
-        }
-    },
-    methods : {
-        changeName : function(){
-            this.name = "Mohini"
-        }
-    }
+// // Component
+// Vue.component('greeting', {
+//     template: '<p>Hello I am {{name}} <button v-on:click="changeName">Click ME</button></p> ',
+//     data: function () {
+//         return {
+//             name: 'MM'
+//         }
+//     },
+//     methods: {
+//         changeName: function () {
+//             this.name = "Mohini"
+//         }
+//     }
+// })
+
+// new Vue({
+//     el: "#component"
+// })
+
+// // refs
+
+// new Vue({
+//     el: '.refs',
+//     data: {
+//         input: ""
+//     },
+//     methods: {
+//         inputRefs: function () {
+//             this.input = this.$refs.input.value
+//         }
+//     }
+// })
+
+
+// data & methods
+
+var obj = {
+    foo: 'bar'
+}
+
+// Object.freeze(obj)
+
+var vm = new Vue({
+    el: '#app',
+    data: obj
 })
-
-new Vue({
-    el : "#component"
-})
-
-// refs
-
-new Vue({
-    el: '.refs',
-    data :{
-        input : ""
-    },
-    methods :{
-        inputRefs : function(){
-            this.input = this.$refs.input.value
-        }
-    }
-})
-
---
+++ meetup-project/meetup/src/App.vue
@@ -0,0 +1,75 @@
+  <template>
+  <v-app>
+    <v-app-bar app class="indigo lighten-4">
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Meet Ups</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn text>
+          <v-icon left>group</v-icon>
+          View Meetups
+        </v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-btn text>
+              <v-icon left>group</v-icon>
+              View Meetups
+            </v-btn>
+          </v-list-tile-action>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+  </v-app>
+  <!-- <v-main>
+    <v-navigation-drawer
+      height="1000"
+      v-model="drawer"
+      class="red"
+      absolute
+      temporary
+    >
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-btn text>
+              <v-icon left>group</v-icon>
+              View Meetups
+            </v-btn>
+          </v-list-tile-action>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar dark>
+      <v-app-bar-nav-icon
+        @click.stop="drawer = !drawer"
+        class="hidden-sm-and-up"
+      ></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Meetup</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn text>
+          <v-icon left>group</v-icon>
+          View Meetups
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+  </v-main> -->
+</template>
+  
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      drawer: false,
+    };
+  },
+
+  components: {},
+};
+</script>
